c142c75f8bda468c4ae2ba6867d25d5f516d3c03
wm <492421512@qq.com>
2020-12-04 14:58:53 +0800

    测试提交

--- juc/src/main/java/com/xykj/juc/JUC/UnsafeListDemo.java
+++ juc/src/main/java/com/xykj/juc/JUC/UnsafeListDemo.java
@@ -14,6 +14,12 @@ import java.util.UUID;
  *      3.1 vector
  *      3.2 Collections.synchronizedList
  *      3.3 CopyOnWriteArrayList
+ *
+ *      写时复制：
+ * CopyOnWrite容器即写时复制的容器。往一个容器添加元素的时候，不直接往当前容器Object[]添加，而是现将当前容器Object[]进行Copy，
+ * 复制出一个新的容器Object[] newElements，然后新的容器Object[] newElements里添加元素，添加完元素之后，
+ * 再将原容器的引用指向新的容器setArray(newElements);。这样做的好处是可以对CopyOnWrite容器进行并发的读，
+ * 而不需要加锁，因为当前容器不会添加任何元素。所以CopyOnWrite容器也是一种读写分离的思想，读和写不同的容器
  */
 public class UnsafeListDemo {
     public static void main(String[] args) {
